97f7549c6b101782dbadf4ff3b90f8dc25f27e26
Ila Barry <barryila20@gmail.com>
2025-04-21 18:22:37 +0000

    titre

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barry ILA | Développeur Full-Stack & Analyste de Données</title>
+    <link rel="icon" type="image" href="img/barry-copie.png">
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
